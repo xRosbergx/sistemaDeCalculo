f80db95db9c863131dfec3ed77bb158afc4e9ea7
Rosberg Guedes <xrosbergx@gmail.com>
2017-02-28 18:27:50 -0300

    Create calcula-imc.js

---
+++ js/calcula-imc.js
@@ -0,0 +1,37 @@
+function calculaTodosImcs(){
+	var trsPacientes = document.getElementsByClassName("paciente");
+
+	percorreArray(trsPacientes, function(pacienteTr){
+	
+		var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+		var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+		var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+
+
+		var pacienteAtual = {nome : tdNome.textContent,
+							 peso : tdPeso.textContent,
+							 altura : tdAltura.textContent,
+							 pegaImc : function(){
+							 	if (this.altura != 0) {
+
+								var imc = (this.peso / (this.altura * this.altura)).toFixed(2);
+								return imc;
+
+								}else {
+
+									console.log('Nao posso dividir por zero');
+
+								}
+
+							 }};
+
+		var imc = pacienteAtual.pegaImc();
+		var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+		tdImc.textContent = imc;
+		console.log(imc);
+	});
+}
+
+function substituiVirgula(campo) {
+	campo.value = campo.value.replace(/,/gi, ".");
+}
